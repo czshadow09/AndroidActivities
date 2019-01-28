b2e787f6f2b841e012e4bd01f23adb3a3559d2e0
czshadow09 <reromarj@gmail.com>
2019-01-28 14:57:40 +0800

    update

--- Blankf7/platforms/browser/www/js/my-app.js
+++ Blankf7/platforms/browser/www/js/my-app.js
@@ -12,40 +12,31 @@ var app = new Framework7({
   // Add default routes
   routes: [
     {
-      path: '/about/',
-      url: 'about.html',
+      path: '/adobo/',
+      url: 'adobo.html',
+    },
+    {
+      path: '/afritada/',
+      url: 'afritada.html',
+    },
+    {
+      path: '/bulalo/',
+      url: 'bulalo.html',
+    },
+    {
+      path: '/kinilaw/',
+      url: 'kinilaw.html',
+    },
+    {
+      path: '/sisig/',
+      url: 'sisig.html',
+    },
+    {
+      path: '/tinola/',
+      url: 'tinola.html',
     },
   ],
   // ... other parameters
-});
-
-var $$ = Dom7;
-
-// DOM events for About popup
-$$('.popup-about').on('popup:open', function (e, popup) {
-  console.log('About popup open');
-});
-$$('.popup-about').on('popup:opened', function (e, popup) {
-  console.log('About popup opened');
-});
-
-// Create dynamic Popup
-var dynamicPopup = app.popup.create({
-  content: '<div class="popup">'+
-              '<div class="block">'+
-                '<p>Popup created dynamically.</p>'+
-                '<p><a href="#" class="link popup-close">Close me</a></p>'+
-              '</div>'+
-            '</div>',
-  // Events
-  on: {
-    open: function (popup) {
-      console.log('Popup open');
-    },
-    opened: function (popup) {
-      console.log('Popup opened');
-    },
-  }
 });
 
 var mainView = app.views.create('.view-main');
